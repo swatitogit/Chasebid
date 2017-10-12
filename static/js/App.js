d1d5c4389ba739fce95f04ae7aa5adc1a0fa5ab1
swatitogit <swatipatil9823@gmail.com>
2017-10-12 18:20:38 +0530

    1.create home page 2.and assign it to home state on index page

--- static/js/App.js
+++ static/js/App.js
@@ -6,6 +6,11 @@ angular.module('chasebidApp', ['chasebidApp.controllers','ui.router'])
 		templateUrl:'../html_templates/SignUpReg.html',
 		controller:'signupCtrl'
 	})
+	.state('home', {
+		url: '/home',
+		templateUrl:'../html_templates/home_page.html',
+		// controller:'signupCtrl'
+	})
 
 	$urlRouterProvider.otherwise('/');
 }); //config ends
